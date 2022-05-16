507e5aef6c642644049957fae42bf2c8b7cd6a6e
1010varun <varunagarwal10.2@gmail.com>
2022-05-17 01:06:14 +0530

    query_connect

---
+++ script.js
@@ -0,0 +1,47 @@
+let a = document.getElementById("submit_button").addEventListener("click", submit);
+
+function submit() {
+    let user = parseInt(document.getElementById("user_id").value);
+    // alert(typeof(user);
+    // let pass = document.getElementById("user_pass").value;
+    let user1 = parseInt(2100291520060);
+    let user2 = parseInt(2100291520059);
+    let user3 = parseInt(2100291520058);
+    let user4 = parseInt(2100291520056);
+    let user5 = parseInt(2100291520057);
+
+    switch (user) {
+        case user1:
+            location.href = "dashboard.html";
+            break;
+
+        case user2:
+            location.href = "dashboard.html";
+            break;
+
+        case user3:
+            location.href = "dashboard.html";
+            break;
+
+        case user4:
+            location.href = "dashboard.html";
+            break;
+
+        case user5:
+            location.href = "dashboard.html";
+            break;
+
+        default:
+            alert("wrong id or password");
+    }
+
+}
+
+function posted() {
+    alert("Posted Succesfully. Will be showm on your dashboard after 24 hrs");
+}
+
+function logout(){
+    location.href = "index.html";
+    alert("Logout Succesfully");
+}
